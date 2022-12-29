f7f32eebcbb3af6a098f4329c5b655502c069561
EnesAhmetGul <100159874+EnesAhmetGul@users.noreply.github.com>
2022-12-29 23:21:08 +0300

    Update QuickSort.java

--- QuickSort/src/quicksort/QuickSort.java
+++ QuickSort/src/quicksort/QuickSort.java
@@ -9,80 +9,90 @@ import java.util.Random;
 public class QuickSort {
     public static void main(String[] args) { 
         int minislem = Integer.MAX_VALUE;
-        int istenenPivotIndex = -1;
-        int n = 100; // Dizinin boyutu
-        int[] array = new int[n];
+        int istenen = -1;
+        int n = 100;
+        int[] dizi = new int[n];
+        
         Random r = new Random();
-        for (int i=0;i<array.length;i++){
-            array[i]=r.nextInt(100);
+        
+        for (int i = 0 ; i < dizi.length ; i++){
+            dizi[i]=r.nextInt(100);
         }
-        // Pivot olarak dizinin yüzde 10, yüzde 20, yüzde 30, yüzde 40, yüzde 50, yüzde 60, yüzde 70, yüzde 80 ve yüzde 90 ve indekslerini seçin
-        int[] pivotIndexleri = {0, 9, 19, 29, 39, 49, 59, 69, 79, 89, 99};
-        for (int pivotIndex : pivotIndexleri) {
-          // Diziyi bölün ve pivot elemanının yerine oturtun
-        int pivot = array[pivotIndex];
+        
+        int[] Indexler = {0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 99};
+        
+        for (int Index : Indexler) {
+        int pivot = dizi[Index];
         int i = 0;
         int j = n - 1;
+        
         while (i <= j) {
-            while (array[i] < pivot) {
+            while (dizi[i] < pivot) {
                 i++;
             }
-            while (array[j] > pivot) {
+            while (dizi[j] > pivot) {
                 j--;
             }
             if (i <= j) {
-                int temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
+                int temp = dizi[i];
+                
+                dizi[i] = dizi[j];
+                dizi[j] = temp;
+                
                 i++;
                 j--;
             }
         }
+        
         int islemsayisi = i;
 
-            //en az işlem sayısını bulun ve istenen pivot degeri bulun 
         if (islemsayisi < minislem) {
             minislem = islemsayisi;
-            istenenPivotIndex = pivotIndex;    
+            istenen = Index;    
         }
-            pivotIndex=pivotIndex+1;
-            System.out.println("Pivot %"+ pivotIndex+" secildiginde islem sayisi:" + islemsayisi);
+            Index = Index+1;
+            System.out.println("Baslangic " + Index + " secildiginde islem sayisi:" + islemsayisi);
         }
 
-        // En az işlem sayısını bulun ve yazdırın
 
-        for (int pivotIndex : pivotIndexleri) {
-          // Diziyi bölün ve pivot elemanının yerine oturtun
-          int pivot = array[pivotIndex];
+        for (int Index : Indexler) {
+            
+          int pivot = dizi[Index];
           int i = 0;
           int j = n - 1;
+          
           while (i <= j) {
-            while (array[i] < pivot) {
+            while (dizi[i] < pivot) {
                 i++;
             }
-            while (array[j] > pivot) {
+            while (dizi[j] > pivot) {
               j--;
             }
                 if (i <= j) {
-                    int temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
+                    int temp = dizi[i];
+                    
+                    dizi[i] = dizi[j];
+                    dizi[j] = temp;
+                    
                     i++;
                     j--;
                 }
             }
-            }
-            System.out.println();
-            System.out.println();
-            istenenPivotIndex=istenenPivotIndex+1;
-            if(istenenPivotIndex==1){
-                System.out.println("Pivot ilk eleman secildiginde islem sayisi en azdir:" + minislem);  
-            }
-            else if(istenenPivotIndex==100){
-                System.out.println("Pivot son eleman secildiginde islem sayisi en azdir:" + minislem); 
-            }
-            else{
-            System.out.println("Pivot % "+ istenenPivotIndex+" secildiginde islem sayisi en azdir:" + minislem);
+        }
+        
+        System.out.println();
+        System.out.println();
+
+        istenen = istenen + 1;
+
+        if(istenen == 1){
+                System.out.println("Ilk eleman secildiginde islem sayisi en azdir");  
+        }
+        else if(istenen == 100){
+                System.out.println("Son eleman secildiginde islem sayisi en azdir"); 
+        }
+        else{
+            System.out.println("Baslangic "+ istenen +" secildiginde islem sayisi en azdir:" + minislem);
         }
     }
     
